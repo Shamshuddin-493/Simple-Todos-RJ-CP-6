33dfcf39192cc3a212b8d145e867b1c71e67e106
Shamshuddin Shaik <shamshuddinis726@gmail.com>
2025-12-19 16:03:41 +0530

    All Test Cases Passed

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,21 +1,22 @@
+// Write your code here
 import './index.css'
 
-const ListItem = props => {
+const TodoItem = props => {
   const {todoDetails, deleteTodo} = props
   const {id, title} = todoDetails
+
   const onDeleteTodo = () => {
     deleteTodo(id)
   }
+
   return (
-    <li>
-      <div>
-        <p>{title}</p>
-        <button type="button" onClick={onDeleteTodo}>
-          Delete
-        </button>
-      </div>
+    <li className="todo-item">
+      <p className="title">{title}</p>
+      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+        Delete
+      </button>
     </li>
   )
 }
 
-export default ListItem
+export default TodoItem
